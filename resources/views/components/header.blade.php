bfa8a2a81690296dccff7353f45c84d93a900676
Josef Forkman <strand.vatten@outlook.com>
2023-03-01 14:17:44 +0100

    add fontawsome and scss

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,10 @@
+<header class="bg-blue">
+    <nav>
+        <ul>
+            <li><a class="text-white" href="/">Home</a></li>
+            <li><a class="text-white" href="/Produkt">Produkt</a></li>
+            <li><a class="text-white" href="/About">About</a></li>
+        </ul>
+
+    </nav>
+</header>
---
+++ resources/views/components/side-menu.blade.php
@@ -0,0 +1,10 @@
+<section class="side-menu bg-blue">
+    <h2 class="text-white">Katogorier</h2>
+    <nav>
+        <ul>
+            <li><a class="text-black" href="">Teck</a></li>
+            <li><a class="text-black" href="">Books</a></li>
+            <li><a class="text-black" href="">Game</a></li>
+        </ul>
+    </nav>
+</section>
